4fcae06a47134649e2afa69a9f06c053ba887864
it20150648 <it20150648@my.sliit.lk>
2024-04-18 02:00:36 +0530

    Change mongodb url env

--- userManagement/server.js
+++ userManagement/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
-const URL = process.env.MONGODB_URL;
+const URL = process.env.CUSTOMCONNSTR_MONGODB_URL ;
 
 mongoose.connect(URL, {
   
