c8f69e0678f48d0be967243be1f705b079216cf9
isuraem <erandaim@gmail.com>
2024-05-09 19:26:57 +0530

    updated_cart_service

--- cartManagement/app/services/cartService.js
+++ cartManagement/app/services/cartService.js
@@ -148,7 +148,7 @@ module.exports.removeItemFromCartService = async (requestBody) => {
 
     // Save the updated cart
     await updatedCart.save();
-    
+
     return {
       msg: "success",
       data: updatedCart,
@@ -194,94 +194,3 @@ module.exports.updateItemQuantityService = async (requestBody) => {
     throw error; // Optionally rethrow the error for handling further up the call stack
   }
 }
-
-// //view Cart service for view all CArt details
-// module.exports.viewCartService = async (req, res) => {
-//   try {
-//     let response = await Cart.find();
-//     // console.log("response", response);
-//     let cartArraye = [];
-
-//     for (let index = 0; index < response.length; index++) {
-//       let id = response[index].itemId;
-//       console.log("id", id);
-//       let productResponse = await Product.findOne({ _id: id })
-//       console.log("productRes", productResponse);
-
-//       if (productResponse) {
-//         let cartObject = {
-//           productDetails: productResponse,
-//           cartDetails: response[index],
-//         }
-
-//         cartArraye.push(cartObject);
-//       }
-
-//     };
-
-//     if (response) {
-//       return {
-//         msg: "success",
-//         data: cartArraye,
-//       };
-//     } else {
-//       return {
-//         msg: "faild",
-//         data: response,
-//       };
-//     }
-//   } catch {
-//     throw err;
-//   }
-// };
-
-// module.exports.updateCartItemService = async (req, res) => {
-//   console.log(">>>>>>>>", req)
-//   try {
-//     let response = await Cart.findOneAndUpdate(
-//       { itemId: req.itemId },
-//       { $set: { cartQuantity: req.cartQuantity } },
-
-//     );
-//     console.log("res>>", response)
-
-//     if (response) {
-//       return {
-//         msg: "success",
-//         data: response,
-//       };
-//     } else {
-//       return {
-//         msg: "fail",
-//         data: null,
-//       };
-//     }
-//   } catch (err) {
-//     throw err;
-//   }
-// };
-
-
-// module.exports.deleteCartItemService = async (req, res) => {
-//   try {
-//     console.log("request", req)
-//     let response = await Cart.findOneAndDelete(
-//       { itemId: req.itemId },
-
-//     );
-
-//     if (response) {
-//       return {
-//         msg: "success",
-//         data: response,
-//       };
-//     } else {
-//       return {
-//         msg: "fail",
-//         data: null,
-//       };
-//     }
-//   } catch (err) {
-//     throw err;
-//   }
-// };
